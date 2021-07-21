0d438c54b15c8bd861ad46e7ef3d88883eee499a
Daniel Rodrigues <87614122+drsjm@users.noreply.github.com>
2021-07-21 23:03:07 +0100

    Update send_email_page.php

--- send_email_page.php
+++ send_email_page.php
@@ -50,6 +50,22 @@
 					<button type="submit" class="btn btn-secondary">Send Message</button>
 				</div>
 			</form>
+
+	<p>
+
+			<strong>If you want to thank me...</strong>
+
+			<br>
+
+			<a href="https://www.buymeacoffee.com/drsjm" target="_blank">
+
+				<img src="https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png" alt="Buy Me A Coffee" style="height: 41px !important;width: 174px !important;box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;-webkit-box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;" >
+
+			</a>
+
+		</p>
+
+
 		</div>
 	
 		<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
